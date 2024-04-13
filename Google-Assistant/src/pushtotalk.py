7a03e5c52ec49a69cc8005a20ef0ec38f920030a
Ted Michalik <35380327+TedMichalik@users.noreply.github.com>
2024-04-13 17:27:27 -0400

    Update pushtotalk.py to try again to fix exit.

--- Google-Assistant/src/pushtotalk.py
+++ Google-Assistant/src/pushtotalk.py
@@ -131,6 +131,7 @@ class SampleAssistant(object):
                     if "exit" in resp.result.spoken_request_text:
                         logging.info('Computer EXIT command detected.')
                         once = True
+                        continue_conversation = False
                 assistantindicator('speaking')
                 logging.info('Playing assistant response.')
             if len(resp.audio_out.audio_data) > 0:
